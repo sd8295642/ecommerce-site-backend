f792466587eef10273e7b71f748e45d32bb60752
sd8295642 <sd8295642@gmail.com>
2024-05-15 17:14:26 -0500

    Completed routes files to correctly get, post, put, or destroy (CRUD) items for Category, Product, Product and ProductTag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,14 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
-    product_id: {
+    id: {//2
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: { //1
       type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
       },
     },
-    tag_id: {
+    tag_id: {// 2
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
